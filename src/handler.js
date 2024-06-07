dfd5d5035fb724d924f1f8381ea3022dcf5d99d3
clvnworld78 <calvincaesar777@gmail.com>
2024-06-08 02:17:24 +0700

    added the approriate handler and routes for the added API function

--- src/handler.js
+++ src/handler.js
@@ -1,54 +1,48 @@
 require('dotenv').config();
-const axios = require('axios');
-const API_KEY = process.env.PLACES_API_KEY;
-
-const getRestaurantList = (req, res) => { // req payload should include location coordinate
-    console.log(`this is get request on ${req.url}`);
-    // get the gmaps API key from .env and insert logic
-    res.status(201).send('Sending back gmaps location details as an object on a json format');
-};
+const api = require('./api/places');
 
 const getHandler = (req, res) => {
     console.log(`This is ${req.method} request on ${req.url}`);
-    res.status(200).send('Hello world!');
-}
+    res.status(200).send('Connection to backend successfully established!');
+};
 
-const searchPlaces = async (req, res) => {
+const searchPlaces = async (req, res) => { // req body must contain location query
     const location = req.query.location;
 
     if (!location) {
         return res.status(400).send('Missing required parameter: location text');
-    }
+    };
 
     try {
-        const response = await axios.get(
-            `https://maps.googleapis.com/maps/api/place/textsearch/json?query=restaurants+near+${location}&key=${API_KEY}`
-        );
-
-        const restaurants = response.data.results.map( place => {
-            const { photos = [] } = place; // Destructure photos with default empty array
-            const photo = photos[0]; // Access the first photo (if available)
-            const photoURL = photo ? `https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=${photo.photo_reference}&key=${API_KEY}` : null;
-
-            return {
-                name: place.name,
-                rating: place.rating,
-                vicinity: place.vicinity,
-                imageUrl: photoURL,
-            };
-        });
-
+        const restaurants = await api.searchPlaces(location);
         res.json(restaurants);
     } catch (error) {
         console.error(error);
         res.status(500).send('Error fetching restaurant data');
-    }
-}
+    };
+};
+
+const fetchNearbyRestaurants = async (req, res) => { // req query must contain latitude & langtitude
+    try {
+        const { lat, lng, radius = 1000 } = req.query;
+
+        if (!lat || !lng || !radius) {
+            return res.status(400).send('Missing one or more required paramater: latitude, langtitude, radius');
+        };
+
+        const location = `${lat},${lng}`;
+        const nearbyRestaurants = await api.fetchNearbyRestaurants(location, radius);
+        res.json(nearbyRestaurants);
+    } catch (error) {
+        console.error('Error during fetching restaurants nearby: ', error);
+        res.status(500).send({ error: 'Failed to fetch restaurants from nearby location' });
+    };
+};
 
 const handlers = {
-    getRestaurantList,
     getHandler,
-    searchPlaces
+    searchPlaces,
+    fetchNearbyRestaurants
 };
 
 module.exports = handlers;
--- src/routes.js
+++ src/routes.js
@@ -2,14 +2,8 @@ const express = require('express');
 const router = express.Router();
 const handlers = require('./handler.js');
 
-router.get('/restaurant', (req, res) => {
-    handlers.getRestaurantList(req, res);
-});
-
-router.get('/', (req, res) => {
-    handlers.getHandler(req, res);
-})
-
+router.get('/', (req, res) => handlers.getHandler(req, res));
 router.get('/search', (req, res) => handlers.searchPlaces(req, res));
+router.get('/nearby', (req, res) => handlers.fetchNearbyRestaurants(req, res));
 
 module.exports = router;
