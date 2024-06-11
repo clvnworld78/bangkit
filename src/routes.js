dc10e35596e84bd52b0fae73899a5c318fbb41cb
clvnworld78 <calvincaesar777@gmail.com>
2024-06-11 23:15:53 +0700

    added the apporiate end point for login and registration

--- src/routes.js
+++ src/routes.js
@@ -6,4 +6,7 @@ router.get('/', (req, res) => handlers.getHandler(req, res));
 router.get('/search', (req, res) => handlers.searchPlaces(req, res));
 router.get('/nearby', (req, res) => handlers.fetchNearbyRestaurants(req, res));
 
+router.post('/register', (req, res) => handlers.registration(req, res));
+router.post('/login', (req, res) => handlers.login(req, res));
+  
 module.exports = router;
