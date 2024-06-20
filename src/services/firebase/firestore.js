d1a156019a7ec5f55f79c186f0c4bb7c340c6727
clvnworld78 <calvincaesar777@gmail.com>
2024-06-20 22:16:09 +0700

    Bug fix

--- src/services/firebase/firestore.js
+++ src/services/firebase/firestore.js
@@ -1,8 +1,7 @@
 const admin = require('firebase-admin');
-const serviceAccount = require('./firebase-admin-bucketreader-sdk.json')
+const serviceAccount = require('./firebase-admin-bucketreader-sdk.json');
 
 admin.initializeApp({ credential: admin.credential.cert(serviceAccount) });
 console.log('Credential successfully loaded');
 const db = admin.firestore();
 module.exports = db;
-  
