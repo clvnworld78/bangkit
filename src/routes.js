fb5773880beb726a2815062033627e64c6637104
clvnworld78 <calvincaesar777@gmail.com>
2024-06-20 22:10:48 +0700

    Added all the required routes

--- src/routes.js
+++ src/routes.js
@@ -5,9 +5,12 @@ const handlers = require('./handler.js');
 router.get('/', (req, res) => handlers.getHandler(req, res));
 router.get('/search', (req, res) => handlers.searchPlaces(req, res));
 router.get('/nearby', (req, res) => handlers.fetchNearbyRestaurants(req, res));
+router.get('/reviewed', (req, res) => handlers.getAllUserRestaurant(req, res));
+router.get('/detail', (req, res) => handlers.getRestaurantDetails(req, res));
+router.post('/statistics', (req, res) => handlers.statistics(req, res));
 
 router.post('/register', (req, res) => handlers.registration(req, res));
 router.post('/login', (req, res) => handlers.login(req, res));
 router.post('/review', (req, res) => handlers.sentimentAnalysis(req, res));
-  
+
 module.exports = router;
